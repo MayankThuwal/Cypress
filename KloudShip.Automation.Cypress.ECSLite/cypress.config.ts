abd320ca0060d0acc0b2df1f2f9441e95434a957
Mayank Thuwal <mayank.thuwal@impledge.com>
2025-11-06 14:29:44 +0530

    Cypress config update

--- KloudShip.Automation.Cypress.ECSLite/cypress.config.ts
+++ KloudShip.Automation.Cypress.ECSLite/cypress.config.ts
@@ -3,23 +3,11 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   viewportWidth: 1366,
   viewportHeight: 639,
-
-  reporter: "cypress-mochawesome-reporter",
-  reporterOptions: {
-    reportDir: "cypress/reports",
-    charts: true,
-    overwrite: false,
-    html: true,
-    json: true,
-  },
-
   e2e: {
     // To override the default browser security and cross-origin issue
     chromeWebSecurity: false,
     supportFile: "cypress/support/e2e.ts",
-    setupNodeEvents(on, config) {
-      require("cypress-mochawesome-reporter/plugin")(on);
-      return config;
+    setupNodeEvents(on,config) {
     },
     // For recoard and play
     experimentalStudio: true,
